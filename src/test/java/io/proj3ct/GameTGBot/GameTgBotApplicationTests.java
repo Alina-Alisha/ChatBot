29eee2488742603f81996bd678f912f8f91b8d46
Samareva18 <p.samareva@mail.ru>
2022-10-24 23:28:42 +0500

    feat: добавлена привязка к телеграму

---
+++ src/test/java/io/proj3ct/GameTGBot/GameTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.GameTGBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
